d0c6e659b654ae3a3de3fcb589344e433645895f
parvez13913 <parvezz13913@gmail.com>
2023-08-20 14:44:58 +0600

    Implement GitHub authentication system with NextAuth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     providers: [
-
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
     ],
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
+  const { data: session } = useSession()
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
+      <h2 style={{ textAlign: "center" }}>User Name: {session?.user?.name}</h2>
     </div>
   );
 };
